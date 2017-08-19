9a16cf4bd7837f97a8a2f73e926b3d35d0e71a5f
anknife <1981856494@qq.com>
2017-08-19 14:27:15 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,20 @@
 <template>
   <div id="app">
-    <some id="search"></some>
-    <foot id="footer"></foot>
-    <menu id="menu"></menu>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import some from './components/search-some.vue'
-  import foot from './components/footer.vue'
-  import menu from './components/Menu.vue'
-  export default {
-    components: {
-      some,
-      foot,
-      menu
-    }
-  }
+export default {
+  name: 'app'
+}
 </script>
 
 <style>
   html{
     height: 100%;
-  }
-
-  body{
-    height: 100%;
-    overflow: hidden;
-    background:url('./assets/background.png') no-repeat;
-    background-size: 100% 100%;
-  }
-
-  #search {
-    color: #2c3e50;
-    width: 600px;
-    font-family: Source Sans Pro, Helvetica, sans-serif;
-    text-align: center;
-    position: absolute;
-    top: 35%;
-    left: 50%;
-    margin-left: -300px;
-    margin-top: -100px;
-  }
-
-  #footer{
-    width: 100%;
-    position: absolute;
-    margin-left: 0;
-    bottom: 0;
+    margin: 0;
+    padding: 0;
   }
 </style>
 
---
+++ src/components/main.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <some id="search"></some>
+    <foot></foot>
+    <menu id="menu"></menu>
+  </div>
+</template>
+
+<script>
+  import some from './search-some.vue'
+  import foot from './footer.vue'
+  import menu from './Menu.vue'
+  export default {
+    name: 'home',
+    components: {
+      some,
+      foot,
+      menu
+    }
+  }
+</script>
+
+<style scoped>
+  #search {
+    color: #2c3e50;
+    width: 600px;
+    font-family: Source Sans Pro, Helvetica, sans-serif;
+    text-align: center;
+    position: absolute;
+    top: 35%;
+    left: 50%;
+    margin-left: -300px;
+    margin-top: -100px;
+  }
+</style>
